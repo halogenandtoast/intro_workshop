2510e556c1e6b00876daa99dbae9bd2d11798d25
Matthew Mongeau <halogenandtoast@gmail.com>
2012-03-27 10:51:19 -0300

    Display user registration form.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Shouter::Application.routes.draw do
   resources :shouts, :only => [:index, :create]
+  resources :users, :only => [:new, :create]
 
   # get "shouts" => "shouts#index"
   # post "shouts" => "shouts#create"
