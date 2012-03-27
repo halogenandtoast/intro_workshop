c331395ec4223453db6463a54831433f97b45eb0
Matthew Mongeau <halogenandtoast@gmail.com>
2012-03-27 11:57:19 -0300

    Saving user id in cookie.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,10 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by_email(params[:session][:email])
+    cookies[:user_id] = user.id
+    redirect_to shouts_path
+  end
 end
