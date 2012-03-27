08ed43d063252bf902ac9e5114674727d8786b8f
Matthew Mongeau <halogenandtoast@gmail.com>
2012-03-27 09:30:31 -0300

    Add time to shouts index

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Shouter::Application.routes.draw do
   resources :shouts, :only => [:index, :create]
+
+  # get "shouts" => "shouts#index"
+  # post "shouts" => "shouts#create"
+  # get "shouts/new" => "shouts#new"
+  # get "shouts/edit" => "shouts#edit"
+  # get "shouts/:id" => "shouts#show"
+  # put "shouts/:id" => "shouts#update"
+  # delete "shouts/:id" => "shouts#destroy"
 end
