87e0923783a3d8c59842fb01d24d88a3473cb8d5
Matthew Mongeau <halogenandtoast@gmail.com>
2012-05-07 17:10:00 -0400

    Persist shouts.

--- 05-07-2012/Shouter/app/controllers/shouts_controller.rb
+++ 05-07-2012/Shouter/app/controllers/shouts_controller.rb
@@ -1,6 +1,12 @@
 class ShoutsController < ApplicationController
   def index
-    @shouts = Shout.all
+    @shouts = Shout.current
     @shout = Shout.new
+  end
+
+  def create
+    @shout = Shout.new(params[:shout])
+    @shout.save
+    redirect_to shouts_url, notice: "Shouted!"
   end
 end
--- 05-07-2012/Shouter/app/models/shout.rb
+++ 05-07-2012/Shouter/app/models/shout.rb
@@ -1,3 +1,7 @@
 class Shout < ActiveRecord::Base
   attr_accessible :body
+
+  def self.current
+    order("created_at DESC")
+  end
 end
